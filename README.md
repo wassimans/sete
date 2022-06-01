baccb8da58f275147db1de3b507fe82f4f474112
Wassim Mansouri <wassim.mansouri@gmail.com>
2022-06-01 13:58:28 +0200

    Add hashing method

--- README.md
+++ README.md
@@ -6,7 +6,12 @@
 
  A Block is a data structure that contains the following:
 
- -- Timestamp in milliseconds
- -- lastHash: the hash of the block before
- -- hash: based on this block's data
- -- the data the block stores
+ - Timestamp in milliseconds
+ - lastHash: the hash of the block before
+ - hash: based on this block's data
+ - the data the block stores
+
+ It contains also:
+ - a static method to generate the genesis block
+ - a static method to mine a new block based on the previous block's hash and given new data
+ - a static method to produce the new block's hash (using the SHA-256 algorithm and based on the crypto-js package)
--- block.js
+++ block.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
@@ -16,7 +18,19 @@ class Block {
     }
 
     static genesis() {
-        return this('Genesis time', '-----', 'f1r5th45h3v3r', []);
+        return new this('Genesis time', '-----', 'f1r5th45h3v3r', []);
+    }
+
+    static mineBlock(lastBlock, data) {
+        const timestamp = Date.now();
+        const lastHash = lastBlock.hash;
+        const hash = Block.hash(timestamp, lastHash, data);
+
+        return new this(timestamp, lastHash, hash, data);
+    }
+
+    static hash(timestamp, lastHash, data) {
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
 }
 
